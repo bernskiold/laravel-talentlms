b963032298d0af957a18752b07aea221fa9a45e5
Ann Lickander <alickander@gmail.com>
2025-06-04 13:15:16 +0200

    wip

---
+++ src/Api/Resources/Groups.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bernskiold\LaravelTalentLms\Api\Resources;
+
+use Bernskiold\LaravelTalentLms\Data\ApiResources\UserCourseEnrollment;
+use Bernskiold\LaravelTalentLms\Data\ApiResources\UserGroupConnection;
+use Bernskiold\LaravelTalentLms\Data\ListResponse;
+use Illuminate\Support\Str;
+
+class Groups extends TalentLmsResource
+{
+
+    public function addUser(int $userId, string $groupKey ): ?UserGroupConnection
+    {
+        $params = [
+            'user_id' => $userId,
+            'group_key' => $groupKey,
+        ];
+
+        $response = $this->client->post("/addUserToGroup", $params);
+
+        if (!$response) {
+            return null;
+        }
+
+        return UserGroupConnection::fromResponse($response);
+    }
+
+    public function removeUser(int $userId, string $groupKey): ?UserGroupConnection
+    {
+        $params = [
+            'user_id' => $userId,
+            'group_key' => $groupKey,
+        ];
+
+        $response = $this->client->post("/removeUserFromGroup", $params);
+
+        if (!$response) {
+            return null;
+        }
+
+        return UserGroupConnection::fromResponse($response);
+    }
+
+}
---
+++ src/Data/ApiResources/UserGroupConnection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bernskiold\LaravelTalentLms\Data\ApiResources;
+
+use Bernskiold\LaravelTalentLms\Support\ApiParsing;
+use Carbon\CarbonInterface;
+
+class UserGroupConnection
+{
+
+    public function __construct(
+        public readonly int    $userId,
+        public readonly int    $groupId,
+        public readonly string $groupName,
+    )
+    {
+    }
+
+    public static function fromResponse(object $response): self
+    {
+        return new self(
+            userId: $response->user_id,
+            groupId: $response->group_id,
+            groupName: $response->group_name ?? '',
+        );
+    }
+
+}
